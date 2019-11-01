5414b48506ccfe0b48b316f02d5e2bd59a45e1f0
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-11-01 15:08:10 -0400

    fixed sidebar dynamically switching from routingForm.js and sidebar-routing.js

--- src/components/sidebar/sidebar-routing/sidebar-routing.js
+++ src/components/sidebar/sidebar-routing/sidebar-routing.js
@@ -1,79 +1,75 @@
-import React, { useState } from 'react'; 
+import React from 'react'; 
 import './sidebar-routing.scss'
-import RoutingForm from '../../map/routingForm'
 import SidebarMenu from '../SidebarMenu.js'
 
 const RoutingSidebar = (props) => {
 
-    const [state, setState] = useState({
-        routingForm: "off"
-    })
-
-    console.log('RoutingSidebar', props)
-    console.log('RoutingSidebar', state.routingForm)
-
     const sidebarAnchor = () => {
-        let sidebar = document.getElementById('overlayNav')
-        sidebar.style.height = '100%'
-        sidebar.style.margin = '0'
-        sidebar.style.width = '375px'
-        document.getElementsByClassName('navbar')[0].style.display = 'none'
-        document.getElementsByClassName('overlay-content')[0].style.margin = 0
+        if(props.state.directions === 'on'){
+            let sidebar = document.querySelector('#overlayNav.overlay.open')
+            sidebar.style.height = '100%'
+            sidebar.style.margin = '0'
+            sidebar.style.width = '375px'
+            document.getElementsByClassName('navbar')[0].style.display = 'none'
+            document.getElementsByClassName('overlay-content')[0].style.margin = 0
+        } else {
+            let sidebar = document.querySelector('#overlayNav.overlay.open')
+            sidebar.style.height = '370px'
+            sidebar.style.margin = '25px'
+            sidebar.style.width = '375px'
+            document.getElementsByClassName('navbar')[0].style.display = 'block'
+            document.getElementsByClassName('overlay-content')[0].style.marginTop = '25px'
+        }
     }
+
     const revertChanges = () => {
-        let sidebar = document.getElementById('overlayNav')
-        sidebar.style.height = '370px !important'
-        sidebar.style.margin = '25px'
-        sidebar.style.width = '375px'
-        document.getElementsByClassName('navbar')[0].style.display = 'block'
-        document.getElementsByClassName('overlay-content')[0].style.marginTop = '25px'
+        props.setState({
+            ...props.state,
+            vehicleForm: "off",
+            routing: "on",
+            vehicles: "off",
+            directions: "off", 
+        })
     }
 
     return (
         <>
             {props.loading !== 'routing successful' ? <p className="route-loading">{props.loading}</p> :
-                state.routingForm === 'off' ? 
-                    <div className='sidebarContainer'>
-                        {/* <div className='sidebarHeader'>
-                            <h2>RV WAY</h2>
-                        </div> */}
-                        <SidebarMenu />
-                        {/* <div className='directionsContainer'> */}
-                            <div className='backbuttonContainer'>
-                                <h6 
-                                className='routingBackButton'
-                                onClick={props.checkState}
-                                >Back</h6>
-                            </div>
-                            <div className='startEndContainer'>
-                                <h3 id='estimatedTime'>17 mins (4 miles)</h3>
-                                <div id='startPointContainer'>
-                                    <p className='startAndEnd'>STARTING POINT</p>
-                                    <p>{props.start}</p>
-                                </div>
-                                <div id='destinationPointContainer'>
-                                    <p className='startAndEnd'>DESTINATION</p>
-                                    <p>{props.end}</p>
-                                </div>
-                            </div>
-                            {/* <div className='sidebarOptions'>
-                                <p>This route avoids</p>
-                            </div> */}
-                            <div className="directions">
-                            <h3 id='directionsTitle'>Directions</h3>
-                                {props.textDirections.map((e, i) => {
-                                    return (
-                                        <p key={i} className="instruction">{e}</p>
-                                        )
-                                    })}
-                                {sidebarAnchor()}
-                            </div>
-                        {/* </div> */}
-                        <div className='sidebarFooterContainer'>
-                            <p id='sidebarFooter'>These directions are for planning purposes only. You may find that construction projects, traffic, weather, or other events may cause conditions to differ from the map results, and you should plan your route accordingly. You must obey all signs or notices regarding your route.</p>
+                <div className='sidebarContainer'>
+                    <SidebarMenu />
+                        <div className='backbuttonContainer'>
+                            <h6 
+                            className='routingBackButton'
+                            onClick={revertChanges}
+                            >Back</h6>
                         </div>
+                        <div className='startEndContainer'>
+                            <h3 id='estimatedTime'>17 mins (4 miles)</h3>
+                            <div id='startPointContainer'>
+                                <p className='startAndEnd'>STARTING POINT</p>
+                                <p>{props.start}</p>
+                            </div>
+                            <div id='destinationPointContainer'>
+                                <p className='startAndEnd'>DESTINATION</p>
+                                <p>{props.end}</p>
+                            </div>
+                        </div>
+                        {/* <div className='sidebarOptions'>
+                            <p>This route avoids</p>
+                        </div> */}
+                        <div className="directions">
+                        <h3 id='directionsTitle'>Directions</h3>
+                            {props.textDirections.map((e, i) => {
+                                return (
+                                    <p key={i} className="instruction">{e}</p>
+                                    )
+                                })}
+                            {sidebarAnchor()}
+                        </div>
+                    <div className='sidebarFooterContainer'>
+                        <p id='sidebarFooter'>These directions are for planning purposes only. You may find that construction projects, traffic, weather, or other events may cause conditions to differ from the map results, and you should plan your route accordingly. You must obey all signs or notices regarding your route.</p>
                     </div>
-                : state.routingForm = 'on'
+                </div>
             }
         </>
     )
